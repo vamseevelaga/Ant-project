d2cc4d335294062ba9cfbd97753372a2d17c811a
Vamsee Krishna <vamsee.velaga@gmail.com>
2018-08-23 23:16:25 -0700

    updated jenkins file name

--- jenkins.groovy
+++ jenkins.groovy
@@ -16,7 +16,7 @@ pipeline {
                 parallel(
                         'Daily VMware Release 1.1.x': {
 			sh("git checkout remotes/origin/ant-rel1")
-		    	sh(" git log --since=1.days > change; if [ -n change ]; then commit=false; export commit; if")
+		    	sh("git log --since=1.days > change; if [ -n change ]; then commit=false; export commit; if")
 		println "the commit value $commit"
 			
                             build job: 'vmware-rel-1.1.x'
